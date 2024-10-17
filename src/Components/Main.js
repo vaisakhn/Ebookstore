2aeb34c69fd43b68476388a17deb3f01cb41cae8
Vaisakh N <142162425+vaisakhn@users.noreply.github.com>
2024-10-17 11:53:37 +0530

    Update Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,7 @@ const Main=()=>{
     const searchBook=(evt)=>{
         if(evt.key==="Enter")
         {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyAAcOkgyBhDgpseVB13iWzT913OUuc-30M')
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))
         }
@@ -24,19 +24,19 @@ const Main=()=>{
         
         <input  value={search} onChange={e=>setSearch(e.target.value)}
                         onKeyDown={searchBook} type="text" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your book name..." required />
-        <button onClick={()=>{ axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyAAcOkgyBhDgpseVB13iWzT913OUuc-30M')
+        <button onClick={()=>{ axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))}} style={{width:'5rem'}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">serach</button>
     </div>
     </div>
 
 <div class='flex'><button onClick={()=> {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'=free-ebooks&key=AIzaSyAAcOkgyBhDgpseVB13iWzT913OUuc-30M')
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'=free-ebooks&key=')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))
         }} type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-slate-400 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Free Books</button>
 <button onClick={()=> {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'=paid-ebooks&key=AIzaSyAAcOkgyBhDgpseVB13iWzT913OUuc-30M')
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'=paid-ebooks&key=')
             .then(res=>setData(res.data.items))
             .catch(err=>console.log(err))
         }} type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-slate-400 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Paid Books</button>
